a596881c2b6e2c1238eb8fc64d35dbc4138ba680
APolubarev <polubarev@gmail.com>
2023-10-14 01:52:21 +0300

    work 5

--- duplicates/Program.cs
+++ duplicates/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
 using System;
 using OfficeOpenXml;
 
@@ -7,16 +8,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        FileInfo fi = new FileInfo(@"c:\Jod\C#\Worm_Gearbox.xlsx");
-        using (ExcelPackage excelPackage = new ExcelPackage(fi))
+        String[] f_names = Directory.GetFiles(@"c:\Jod\Base\Indastra\Файлы загрузки\");
+        //FileInfo fi = new FileInfo(@"c:\Jod\C#\Worm_Gearbox.xlsx");
+
+        foreach (string f_name in f_names)
         {
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            ExcelWorksheet firstWorksheet = excelPackage.Workbook.Worksheets[1];
+            FileInfo f_i = new FileInfo(f_name);
+            using (ExcelPackage excelPackage = new ExcelPackage(f_i))
+            {
+                
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                ExcelWorksheet firstWorksheet = excelPackage.Workbook.Worksheets[0];
 
 
-            string valB1 = firstWorksheet.Cells[1, 2].Value.ToString();
-            Console.WriteLine("В ячейке: " + valB1);
-            
+                string valB1 = firstWorksheet.Cells[1, 2].Value.ToString();
+                Console.WriteLine("В ячейке: " + valB1);
+
+            }
+
+
         }
     }
 }
