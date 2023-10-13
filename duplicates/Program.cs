c8e9a0b99d10d4ba14f08f7d8ab762600e78b107
APolubarev <polubarev@gmail.com>
2023-10-14 01:05:01 +0300

    work 1

--- duplicates/Program.cs
+++ duplicates/Program.cs
@@ -1,5 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int x = 10;
-int y = 2 + x + 5;
-Console.WriteLine("Приветт!");
-Console.WriteLine("Hello, World!");
+using System;
+using OfficeOpenXml;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FileInfo fi = new FileInfo(@"c:\Jod\C#\Worm_Gearbox.xlsx");
+        using (ExcelPackage excelPackage = new ExcelPackage(fi))
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ExcelWorksheet firstWorksheet = excelPackage.Workbook.Worksheets[1];
+
+
+            string valB1 = firstWorksheet.Cells[1, 2].Value.ToString();
+            Console.WriteLine("В ячейке: " + valB1);
+            
+        }
+    }
+}
