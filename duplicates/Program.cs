7169be1fe896cb3ac5c88eda24adf2f662d82fdf
APolubarev <polubarev@gmail.com>
2023-10-14 04:00:30 +0300

    one file publish

--- duplicates/Program.cs
+++ duplicates/Program.cs
@@ -11,20 +11,18 @@ internal class Program
     {
         Console.WriteLine(DateTime.Now);
 
-            string SettingsfileName = "Settings.json";
-            string jsonString = File.ReadAllText(SettingsfileName);
-            Settings CurSettings = JsonSerializer.Deserialize<Settings>(jsonString)!;
+        string SettingsfileName = "Settings.json";
+        string jsonString = File.ReadAllText(SettingsfileName);
+        Settings CurSettings = JsonSerializer.Deserialize<Settings>(jsonString)!;
 
-        // String[] f_names = Directory.GetFiles(@"c:\Jod\Base\Indastra\Файлы загрузки\");
         String[] f_names = Directory.GetFiles(CurSettings.dir);
-        //FileInfo fi = new FileInfo(@"c:\Jod\C#\Worm_Gearbox.xlsx");
 
         foreach (string f_name in f_names)
         {
             FileInfo f_i = new FileInfo(f_name);
             using (ExcelPackage excelPackage = new ExcelPackage(f_i))
             {
-                
+
                 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
                 ExcelWorksheet firstWorksheet = excelPackage.Workbook.Worksheets[0];
 
@@ -39,7 +37,8 @@ internal class Program
         Console.WriteLine(DateTime.Now);
     }
 }
-public class Settings{
+public class Settings
+{
     public string dir { get; set; }
-    
+
 }
